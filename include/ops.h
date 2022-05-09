54546283cfc6e54cbba077f5ce412198f4c0dcd2
Nikolaus J. Reichert <nikolaus@njreichert.ca>
2022-05-08 23:51:27 -0500

    Add various functions, off of <cmath>.

--- include/ops.h
+++ include/ops.h
@@ -11,6 +11,7 @@
 
 #include <unordered_map>
 #include <string>
+#include <cmath>
 
 #include "defs.h"
 
@@ -29,16 +30,35 @@
 /* Two-argument functions */
 /* TODO */
 
-/* TODO: Possibly relocate as an externally-linked variable? */
-const std::unordered_map<std::string, OneFunc> one_arg_ops = {};
+/* TODO: Possibly relocate as an externally-linked variable? also set up namespaces? */
+const std::unordered_map<std::string, OneFunc> one_arg_ops = {
+    {"sqrt", std::sqrt},
+    {"exp", std::exp},
+    {"sin", std::sin},
+    {"cos", std::cos},
+    {"tan", std::tan},
+    {"arcsin", std::asin},
+    {"arccos", std::acos},
+    {"arctan", std::atan},
+    {"log", std::log10}, /* Yes, log10. log_e == ln. */
+    {"ln", std::log},
+    {"log2", std::log2},
+    {"abs", std::abs},
+    {"inv", [](OpType a){ return (1 / a); }},
+    {"nroot", [](OpType a, OpType b){ return std::pow(b, (1 / a)); }}
+};
 
 const std::unordered_map<std::string, TwoFunc> two_arg_ops = {
     {"+", [](OpType a, OpType b){ return a + b; }},
     {"-", [](OpType a, OpType b){ return a - b; }},
     {"*", [](OpType a, OpType b){ return a * b; }},
-    {"/", [](OpType a, OpType b){ return a / b; }}
-};
-
-
+    {"/", [](OpType a, OpType b){ return a / b; }},
+    /*
+     * std::pow returns x^y. Since we are working off a stack, we want y^x
+     * (so the power is at the top of the stack.
+     */
+    {"^", [](OpType a, OpType b){ return std::pow(b, a)}}
+}
 
 #endif
+
