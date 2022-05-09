fc8e6fc628299667d23f8bc1467ccbbe0bb1b1c5
Nikolaus J. Reichert <nikolaus@njreichert.ca>
2022-05-09 00:28:26 -0500

    Add stack operations.

--- include/defs.h
+++ include/defs.h
@@ -18,8 +18,11 @@ typedef double OpType;
 /* Functions that act on only one operand on the stack (negation, sqrt, etc... */
 typedef OpType (*OneFunc)(OpType);
 
-/* Likewise, functions that take two operands (+ - * /) */
+/* Likewise, functions that take two operands (+ - * /)... */
 typedef OpType (*TwoFunc)(OpType, OpType);
+
+/* And operations on the stack itself. */
+typedef void (*StackOp)(std::vector<OpType> &);
 
 #endif
 
--- include/ops.h
+++ include/ops.h
@@ -12,6 +12,7 @@
 #include <unordered_map>
 #include <string>
 #include <cmath>
+#include <vector>
 
 #include "defs.h"
 
@@ -32,20 +33,19 @@
 
 /* TODO: Possibly relocate as an externally-linked variable? also set up namespaces? */
 const std::unordered_map<std::string, OneFunc> one_arg_ops = {
-    {"sqrt", std::sqrt},
-    {"exp", std::exp},
-    {"sin", std::sin},
-    {"cos", std::cos},
-    {"tan", std::tan},
-    {"arcsin", std::asin},
-    {"arccos", std::acos},
-    {"arctan", std::atan},
-    {"log", std::log10}, /* Yes, log10. log_e == ln. */
-    {"ln", std::log},
-    {"log2", std::log2},
-    {"abs", std::abs},
-    {"inv", [](OpType a){ return (1 / a); }},
-    {"nroot", [](OpType a, OpType b){ return std::pow(b, (1 / a)); }}
+    {"sqrt", [](OpType a){ return std::sqrt(a); }},
+    {"exp", [](OpType a){ return std::exp(a); }},
+    {"sin", [](OpType a){ return std::sin(a); }},
+    {"cos", [](OpType a){ return std::cos(a); }},
+    {"tan", [](OpType a){ return std::tan(a); }},
+    {"arcsin", [](OpType a){ return std::asin(a); }},
+    {"arccos", [](OpType a){ return std::acos(a); }},
+    {"arctan", [](OpType a){ return std::atan(a); }},
+    {"log", [](OpType a){ return std::log10(a); }}, /* Yes, log10. log_e == ln. */
+    {"ln", [](OpType a){ return std::log(a); }},
+    {"log2", [](OpType a){ return std::log2(a); }},
+    {"abs", [](OpType a){ return std::abs(a); }},
+    {"inv", [](OpType a){ return (1 / a); }}
 };
 
 const std::unordered_map<std::string, TwoFunc> two_arg_ops = {
@@ -53,12 +53,25 @@ const std::unordered_map<std::string, TwoFunc> two_arg_ops = {
     {"-", [](OpType a, OpType b){ return a - b; }},
     {"*", [](OpType a, OpType b){ return a * b; }},
     {"/", [](OpType a, OpType b){ return a / b; }},
+    
     /*
      * std::pow returns x^y. Since we are working off a stack, we want y^x
      * (so the power is at the top of the stack.
      */
-    {"^", [](OpType a, OpType b){ return std::pow(b, a)}}
-}
+    /* TODO: Find a way to combine these two, as they're just semantics. */
+    {"^", [](OpType a, OpType b){ return std::pow(b, a); }},
+    {"pow", [](OpType a, OpType b){ return std::pow(b, a); }},
+    {"nroot", [](OpType a, OpType b){ return std::pow(b, (1 / a)); }}
+};
+
+const std::unordered_map<std::string, StackOp> stack_ops = {
+    {"clear", [](std::vector<OpType> &vec){ vec.clear(); }}
+};
+
+const std::unordered_map<std::string, OpType> constants = {
+    {"pi", std::asin(1) * 2},
+    {"e", std::exp(1)}
+};
 
 #endif
 
