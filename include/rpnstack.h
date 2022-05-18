46a55fb6d538b82d5a1b807e8de3100fa08aa184
Nikolaus J. Reichert <nikolaus@njreichert.ca>
2022-05-18 00:32:48 -0500

    Add definitions for a distinct RPN Stack datatype.

---
+++ include/rpnstack.h
@@ -0,0 +1,51 @@
+/**
+ * rpnstack.h
+ *
+ * By Nikolaus J. Reichert <nikolaus@njreichert.ca>
+ *
+ * Definitions for an RPN Stack class.
+ */
+
+#include <vector>
+#include <string>
+
+#include "ops.h"
+
+/**
+ * TODO: Doxygen Comments for classes.
+ */
+class RPNStack {
+private:
+    /* 
+     * TODO: Make this function template oriented!
+     *
+     * Some things that need to be figured out:
+     * 1. How to deal with functions in the standard library that may
+     *    or may not support types associated with the template.
+     */
+    std::vector<double> m_stack;
+    
+    bool m_is_error;
+    bool m_is_radians; /* I only intend to support radians or degrees. */
+
+public:
+    /**
+     * @brief Attempts to apply the given one-argument function to the stack.
+     *
+     * @throws TODO: Figure out what to throw here.
+     * @param s The token to parse a function against.
+     *
+     * @returns The value at the top of the stack after the operation completes.
+     */
+    double apply_input(const std::string &s);
+
+    /**
+     * @brief Getter for m_is_error.
+     */
+    bool is_error() const;
+
+    /**
+     * @brief Getter for m_is_radians.
+     */
+    bool is_radians() const;
+}
