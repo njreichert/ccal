c5b64b2b048af67d47a2660cc1cd834b7751db19
Nikolaus J. Reichert <nikolaus@njreichert.ca>
2022-05-18 20:43:11 -0500

    Add sourc file for RPNStack.

---
+++ src/rpnstack.cpp
@@ -0,0 +1,18 @@
+/**
+ * rpnstack.cpp
+ *
+ * By Nikolaus J. Reichert <nikolaus@njreichert.ca>
+ *
+ * Methods and related functions for the RPNStack function.
+ */
+
+bool is_error() const
+{
+    return this->m_is_error;
+}
+
+bool is_radians() const
+{
+    return this->m_is_radians;
+}
+
